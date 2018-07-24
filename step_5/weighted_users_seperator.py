445092215ad7714582e9a663ff6ab0fd67a532d9
vinaybommana <vinaybommana7@gmail.com>
2018-07-24 08:32:13 +0530

    added step 5

--- step_5/weighted_users_seperator.py
+++ step_5/weighted_users_seperator.py
@@ -1,33 +1,36 @@
 import csv
 import codecs
 
+
 def read_csv():
+    rows = list()
     with open("step_four_output.csv", "r") as c:
         csvreader = csv.reader(c)
         next(csvreader)
         for row in csvreader:
             rows.append(row)
+    return rows
 
 
 def main():
-    read_csv()
+    rows = read_csv()
     for i in rows:
-        if float(i[5]) > 1.5:
+        if float(i[4]) > 1.5:
             weighted_rows.append(i)
 
     with codecs.open('step_five_output.csv', 'w+', 'utf-8') as o:
-        o.write("Serial_number\t" + "," + "screen_name\t" + "," + "user_id\t\t" + "," + "No of Tweets" + "," +
+        o.write("Serial_number\t" + "," + "screen_name\t" + "," + "No of Tweets" + "," +
                 "No of retweets" + "," + "weighted_users\n")
         count = 1
         for row in weighted_rows:
             line = str(count) + "," + str(row[1]) + "," + str(row[2]) + "," + str(row[3]) + "," + \
-                   str(row[4]) + "," + str(row[5]) + "\n"
+                   str(row[4])+ "\n"
             o.write(line)
             count += 1
 
 
-rows = list()
 weighted_rows = list()
+
 
 if __name__ == "__main__":
     main()
