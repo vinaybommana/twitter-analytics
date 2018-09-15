7a15235880f9c44861b102451867389c623933bf
vinaybommana <vinaybommana7@gmail.com>
2018-09-16 01:20:13 +0530

    completed step 11

--- step_11/give_tables_for_clusters.py
+++ step_11/give_tables_for_clusters.py
@@ -7,25 +7,101 @@ cluster 0
 | ----         |  ------ | -------      | --------    | ------------------------    |
 |              |         |              |             |                             |
 '''
+from cluster_no_of_tweets_table import read_json
+import csv
+
+
+def read_csv(filename):
+    """
+    :input: filename of the csv file to read from
+    :return: list of rows
+    """
+    rows = list()
+    with open(filename, "r") as f:
+        csvreader = csv.reader(f)
+        next(csvreader)
+        for row in csvreader:
+            rows.append(row)
+    return rows
+
+
+def make_dict_of_cluster_n_tweet_ids(cluster_dictionary, ids_dictionary):
+    """
+    :input:
+    cluster_dictionary --> dictionary containing cluster_index
+                           and list_of_indexes in the cluster
+    ids_dictionary --> dictionary containing index_num and tweet_id
+    :return: <class 'dict'> containing cluster_index and list of tweet_ids
+    """
+    dict_of_cluster_n_tweet_ids = dict()
+
+    for key, value in cluster_dictionary.items():
+        list_of_tweet_ids = list()
+        for index in value:
+            list_of_tweet_ids.append(ids_dictionary[index])
+
+        dict_of_cluster_n_tweet_ids[key] = list_of_tweet_ids
+
+    return dict_of_cluster_n_tweet_ids
+
+
+def make_tweet_rows_dict(ninth_output_rows):
+    """
+    :input: list of step_nine_output.csv rows
+    :return:
+    # this dictionary is to identify the rows of tweets from
+    # just the tweet_id
+    # {
+    #  '<tweet_id>': [<rows respective of tweet_id>] 
+    # }
+    """
+    output_dict = dict()
+    for row in ninth_output_rows:
+        output_dict[str(row[4])] = row
+
+    return output_dict
 
 
 def main():
-    # take clusters and get the tweet_ids from each cluster
-    # for this we have
-    # cluster_nums and index containing json
-    # index and tweet_id containing json
-    # first take each cluster seperate out the indexes
-    # next for the index get the tweet_id
-    # thus at the end there should be a dictionary containing
-    # cluster_number and tweet_ids
+    dict_of_full_index = read_json(file_name='./full_tweets/cluster_num_n_index.json')
+    dict_of_three_fifth_indexes = read_json(file_name='./three_fifth_of_the_tweets/cluster_num_n_index_3_5.json')
+    dict_of_index_ids = read_json(file_name='./full_tweets/indices_ids.json')
+    dict_of_index_ids_3_5 = read_json(file_name='./three_fifth_of_the_tweets/indices_ids_3_5.json')
+
+    nine_output_rows = read_csv('step_nine_with_id_output.csv')
+    tweet_and_rows = make_tweet_rows_dict(nine_output_rows)
+    # -------------------------------------
+    # full tweets
+    # -------------------------------------
+
+    dict_of_cluster_n_tweet_ids = make_dict_of_cluster_n_tweet_ids(dict_of_full_index, dict_of_index_ids)
+    # print(dict_of_cluster_n_tweet_ids)
+    for key, value in dict_of_cluster_n_tweet_ids.items():
+        with open("cluster_" + str(key) + ".csv", "w+") as c:
+            csvwriter = csv.writer(c)
+            csvwriter.writerow(['user@mention', 'user_id', 'No of tweets', 'tweet_id', 'retweet_count', 'local influence score',
+                                'Tweet'])
+            for tweet_id in value:
+                rows = tweet_and_rows[tweet_id]
+                csvwriter.writerow(rows[1:])
 
 
-    # for full tweets
+    # -------------------------------------
+    # three fifth of tweets
+    # ------------------------------------
 
-    # for three_fifth_tweets
-    pass
+    dict_clus_n_twt_ids_3_5 = make_dict_of_cluster_n_tweet_ids(dict_of_three_fifth_indexes, dict_of_index_ids_3_5)
+    # print(dict_clus_n_twt_ids_3_5)
+
+    for key, value in dict_clus_n_twt_ids_3_5.items():
+        with open("three_fifth_cluster_" + str(key) + ".csv", "w+") as c:
+            csvwriter = csv.writer(c)
+            csvwriter.writerow(['user@mention', 'user_id', 'No of tweets', 'tweet_id', 'retweet_count', 'local influence score',
+                                'Tweet'])
+            for tweet_id in value:
+                rows = tweet_and_rows[tweet_id]
+                csvwriter.writerow(rows[1:])
 
 
 if __name__ == "__main__":
     main()
-
---
+++ step_11/order_clusters.py
@@ -0,0 +1,35 @@
+'''
+read every cluster_<num>.csv in full and three_fifth
+arrange based on local influential score
+'''
+
+import csv
+from give_tables_for_clusters import read_csv
+
+
+def main():
+    for i in range(10):
+        rows = read_csv('./full_tweets/cluster_' + str(i) + '.csv')
+        rows = sorted(rows, key=lambda x: float(x[5]), reverse=True)
+        with open('cluster_' + str(i) + '_with_rank.csv', 'w') as c:
+            c.write("Descending order of local infl score" + "," + "user@mention" + "," + "Rank\n")
+            count = 1
+            for row in rows:
+                line = str(row[5]) + "," + str(row[0]) + "," + str(count) + "\n"
+                c.write(line)
+                count += 1
+
+    for i in range(10):
+        rows = read_csv('./three_fifth_of_the_tweets/three_fifth_cluster_' + str(i) + '.csv')
+        rows = sorted(rows, key=lambda x: float(x[5]), reverse=True)
+        with open('three_fifth_cluster_' + str(i) + '_with_rank.csv', 'w') as c:
+            c.write("Descending order of local infl score" + "," + "user@mention" + "," + "Rank\n")
+            count = 1
+            for row in rows:
+                line = str(row[5]) + "," + str(row[0]) + "," + str(count) + "\n"
+                c.write(line)
+                count += 1
+
+if __name__ == "__main__":
+    main()
+
